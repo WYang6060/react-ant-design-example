6c5bdaf71c891502bdd57d4f51e949ea9684f295
Wesley Yang <57732129+WYang6060@users.noreply.github.com>
2021-08-12 12:00:51 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import "./styles.css";
+import "antd/dist/antd.css";
+import { Button, DatePicker, version, Typography, Select } from "antd";
 
 export default function App() {
+  const { Option } = Select;
+  const { Title } = Typography;
+
+  const children = [...Array(1000).keys()].map((idx) => (
+    <Option key={idx.toString(36) + 1}>{idx.toString(36) + 1}</Option>
+  ));
+
+  const handleChange = (val) => console.log(`Selected ${val}`);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="calendar">
+        <h1>Antd version: {version}</h1>
+        <DatePicker />
+        <Button type="primary" style={{ marginLeft: 8 }}>
+          Primary Button
+        </Button>
+      </div>
+
+      <div className="selector">
+        <Title level={2}>{children.length} Items</Title>
+        <Select
+          mode="multiple"
+          style={{ width: "100%" }}
+          placeholder="Please select"
+          defaultValue="a10"
+          onChange={handleChange}
+        >
+          {children}
+        </Select>
+      </div>
     </div>
   );
 }
